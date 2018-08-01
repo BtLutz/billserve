7b7cab30a940f5883ee4c24468c3e60f6b56a99c
BtLutz <brian.lutz9@icloud.com>
2018-07-31 23:26:29 -0400

    Changing max length of legislative subject from 50 to 100.

--- models.py
+++ models.py
@@ -154,7 +154,7 @@ class PolicyArea(models.Model):
 
 
 class LegislativeSubject(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
